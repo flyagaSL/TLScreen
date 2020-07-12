9b49949c1aa3460f8d81ee097cf2173a07872bf1
Sergey <46860359+killer544462@users.noreply.github.com>
2020-07-12 22:51:34 +0300

    Refactoring

--- TLScreen/main.py
+++ TLScreen/main.py
@@ -9,114 +9,96 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
 DOWNLOAD_FOLDER = "D:\\Programs\\TLScreen\\TLScreen\\Telegram\\"
-
 MAX_MESSAGES = 1000
-
 EXTENSIONS_DOCUMENT = [".docx", ".txt"]
-
-url = "https://web.telegram.org/#/im"
-fp = webdriver.FirefoxProfile(os.environ["MProfile"])
-fp.set_preference("browser.download.folderList", 2)
-fp.set_preference("browser.download.manager.showWhenStarting", False)
-fp.set_preference("browser.download.dir",
-                  "{}TmpDownload".format(DOWNLOAD_FOLDER))
-fp.set_preference("browser.helperApps.alwaysAsk.force", False)
-fp.set_preference('browser.helperApps.neverAsk.saveToDisk',
-                  "application/octet-stream")
-
-driver = webdriver.Firefox(fp)
-
-wait = WebDriverWait(driver, 20)
-
-driver.get(url)
-
-chats = wait.until(EC.presence_of_element_located(
-    (By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/ul/li[1]")))
-
-button_menu = wait.until(EC.presence_of_element_located(
-    (By.XPATH, "/html/body/div[1]/div[1]/div/div/div[1]/div/a/div")))
-
-button_menu.click()
-
-button_settings = wait.until(EC.presence_of_element_located(
-    (By.XPATH, "/html/body/div[1]/div[1]/div/div/div[1]/div/ul/li[3]/a")))
-
-button_settings.click()
-
-button_active_sessions = wait.until(EC.presence_of_element_located(
-    (By.XPATH, "/html/body/div[5]/div[2]/div/div/div[3]/div/div[4]/div[3]/a")))
-
-if not os.path.isdir(DOWNLOAD_FOLDER):
-    os.mkdir(DOWNLOAD_FOLDER)
-
-img = ImageGrab.grab()
-img.save(DOWNLOAD_FOLDER + 'profile.jpg')
-
-button_active_sessions.click()
-
-active_sessions = wait.until(EC.presence_of_element_located(
-    (By.XPATH, "/html/body/div[6]/div[2]/div/div/div[2]/div/div/div[1]/ul")))
-
-img = ImageGrab.grab()
-img.save(DOWNLOAD_FOLDER + 'active_essions.jpg')
-
-driver.find_element_by_xpath(
-    "/html/body/div[6]/div[2]/div/div/div[1]/div[1]/div/a").click()
-driver.find_element_by_xpath(
-    "/html/body/div[5]/div[2]/div/div/div[1]/div[1]/div[1]/a[1]").click()
-
-slider = driver.find_element_by_xpath(
-    "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div")
-
-height_chats = driver.find_element_by_xpath(
-    "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]").size["height"]
-
-slider_y_position = slider.location["y"]
-
-img = ImageGrab.grab()
-img.save(DOWNLOAD_FOLDER + "chats_0.jpg")
+URL = "https://web.telegram.org/#/im"
 
 
-count = 1
+def create_firefox_profile():
+    fp = webdriver.FirefoxProfile(os.environ["MProfile"])
+    fp.set_preference("browser.download.folderList", 2)
+    fp.set_preference("browser.download.manager.showWhenStarting", False)
+    fp.set_preference("browser.download.dir",
+                      "{}TmpDownload".format(DOWNLOAD_FOLDER))
+    fp.set_preference("browser.helperApps.alwaysAsk.force", False)
+    fp.set_preference('browser.helperApps.neverAsk.saveToDisk',
+                      "application/octet-stream")
+    return fp
 
-'''
-while True:
-    print(height_chats * count)
-    driver.execute_script(
-        "document.getElementsByClassName(\"im_dialogs_scrollable_wrap  nano-content\")[0].scroll({}, {})".format(0, height_chats * count * 0.9))  # 70% от высоты диалога
 
-    print("old: {} new: {}".format(slider_y_position, slider.location["y"]))
+def screen_object_profile(driver):
+    chats = wait.until(EC.presence_of_element_located(
+        (By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/ul/li[1]")))
 
-    if slider_y_position == slider.location["y"]:
-        break
+    button_menu = wait.until(EC.presence_of_element_located(
+        (By.XPATH, "/html/body/div[1]/div[1]/div/div/div[1]/div/a/div")))
+
+    button_menu.click()
+
+    button_settings = wait.until(EC.presence_of_element_located(
+        (By.XPATH, "/html/body/div[1]/div[1]/div/div/div[1]/div/ul/li[3]/a")))
+
+    button_settings.click()
+
+    button_active_sessions = wait.until(EC.presence_of_element_located(
+        (By.XPATH, "/html/body/div[5]/div[2]/div/div/div[3]/div/div[4]/div[3]/a")))
+
+    if not os.path.isdir(DOWNLOAD_FOLDER):
+        os.mkdir(DOWNLOAD_FOLDER)
+
+    img = ImageGrab.grab()
+    img.save(DOWNLOAD_FOLDER + 'profile.jpg')
+
+    button_active_sessions.click()
+
+    active_sessions = wait.until(EC.presence_of_element_located(
+        (By.XPATH, "/html/body/div[6]/div[2]/div/div/div[2]/div/div/div[1]/ul")))
+
+    img = ImageGrab.grab()
+    img.save(DOWNLOAD_FOLDER + 'active_essions.jpg')
+
+    driver.find_element_by_xpath(
+        "/html/body/div[6]/div[2]/div/div/div[1]/div[1]/div/a").click()
+    driver.find_element_by_xpath(
+        "/html/body/div[5]/div[2]/div/div/div[1]/div[1]/div[1]/a[1]").click()
+
+
+def screen_headers_chats(driver):
+    slider = driver.find_element_by_xpath(
+        "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div")
+
+    height_chats = driver.find_element_by_xpath(
+        "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]").size["height"]
 
     slider_y_position = slider.location["y"]
-    time.sleep(0.5)
+
     img = ImageGrab.grab()
-    img.save(DOWNLOAD_FOLDER + "chats{}.jpg".format(count))
-    count = count + 1
-'''
+    img.save(DOWNLOAD_FOLDER + "chats_0.jpg")
+
+    count = 1
+
+    while True:
+        print(height_chats * count)
+        driver.execute_script(
+            "document.getElementsByClassName(\"im_dialogs_scrollable_wrap  nano-content\")[0].scroll({}, {})".format(0, height_chats * count * 0.9))  # 70% от высоты диалога
+
+        print("old: {} new: {}".format(
+            slider_y_position, slider.location["y"]))
+
+        if slider_y_position == slider.location["y"]:
+            break
+
+        slider_y_position = slider.location["y"]
+        time.sleep(0.5)
+        img = ImageGrab.grab()
+        img.save(DOWNLOAD_FOLDER + "chats{}.jpg".format(count))
+        count = count + 1
+
+    driver.execute_script(
+        "document.getElementsByClassName(\"im_dialogs_scrollable_wrap  nano-content\")[0].scroll({}, {})".format(0, 0))
 
 
-driver.execute_script(
-    "document.getElementsByClassName(\"im_dialogs_scrollable_wrap  nano-content\")[0].scroll({}, {})".format(0, 0))
-
-chat_number = 0
-
-
-while True:
-    chat_number = chat_number + 1
-    print("Выполненяется диалог: {}".format(chat_number))
-
-    badge_chat = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/ul/li[{}]/a/div[1]/span".format(chat_number))
-
-    if badge_chat.text != "":
-        continue
-
-    chat = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/ul/li[{}]".format(chat_number))
-    chat.click()
+def screen_chat(driver):
     chat_header = wait.until(EC.presence_of_element_located(
         (By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/a")))
     chat_header.click()
@@ -124,11 +106,10 @@ while True:
     chat_name = driver.find_element_by_xpath(
         "/html/body/div[5]/div[2]/div/div/div[1]/div[2]/div[2]/div[1]").text
 
-    path_chat_folder = "{}{}".format(
-        DOWNLOAD_FOLDER, chat_name)
+    path_chat_folder = "{}{}".format(DOWNLOAD_FOLDER, chat_name)
 
     if os.path.isdir(path_chat_folder):
-        path_chat_folder = path_chat_folder + "_{}".format(uuid.uuid1())
+        path_chat_folder = path_chat_folder + "_{}".format(uuid.uuid4())
 
     path_chat_folder = path_chat_folder + "\\"
 
@@ -175,14 +156,12 @@ while True:
         "/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]").size["height"]
 
     img = ImageGrab.grab()
-    img.save(path_chat_folder +
-             "chat_0.jpg")
+    img.save(path_chat_folder + "chat_0.jpg")
     screen_number = 1
     while True:
         scroll_height = chat_height * screen_number * 0.9
         driver.execute_script(
             "document.getElementsByClassName(\"im_history_scrollable_wrap\")[0].scroll({}, {})".format(0, scroll_height))
-
         time.sleep(0.5)
         img = ImageGrab.grab()
         img.save(path_chat_folder + "\\chat_{}.jpg".format(screen_number))
@@ -202,8 +181,8 @@ while True:
 
     download_documents = 0
     for document in documents:
-        extension_document = document.find_element_by_xpath(
-            ".").find_element_by_class_name("im_message_document_name").get_attribute("data-ext")
+        extension_document = document.find_element_by_xpath(".").find_element_by_class_name(
+            "im_message_document_name").get_attribute("data-ext")
 
         if extension_document in EXTENSIONS_DOCUMENT:
             document.find_element_by_css_selector(
@@ -211,14 +190,42 @@ while True:
             download_documents = download_documents + 1
 
     count_documents = len(audios) + download_documents
-
     while True:
         if len(os.listdir("{}TmpDownload".format(DOWNLOAD_FOLDER))) == count_documents:
             break
         else:
             time.sleep(0.5)
-    for document in os.listdir("{}TmpDownload".format(DOWNLOAD_FOLDER)):
-        os.rename("{}TmpDownload\\{}".format(DOWNLOAD_FOLDER, document),
-                  path_chat_folder + "\\{}".format(document))
+    for file in os.listdir("{}TmpDownload".format(DOWNLOAD_FOLDER)):
+        os.rename("{}TmpDownload\\{}".format(DOWNLOAD_FOLDER, file),
+                  path_chat_folder + "\\{}".format(file))
 
-print("Выполнение программы успешно завершено!!!")
+
+def screen_chats(driver):
+    chat_number = 0
+    while True:
+        chat_number = chat_number + 1
+        print("Выполненяется диалог: {}".format(chat_number))
+
+        badge_chat = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/ul/li[{}]/a/div[1]/span".format(chat_number))
+
+        if badge_chat.text != "":
+            continue
+
+        chat = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/ul/li[{}]".format(chat_number))
+        chat.click()
+        screen_chat(driver)
+
+
+if __name__ == "__main__":
+    if not os.path.isdir("{}TmpDownload".format(DOWNLOAD_FOLDER)):
+        os.mkdir("{}TmpDownload".format(DOWNLOAD_FOLDER))
+
+    driver = webdriver.Firefox(create_firefox_profile())
+    wait = WebDriverWait(driver, 20)
+    driver.get(URL)
+    screen_object_profile(driver)
+    screen_headers_chats(driver)
+    screen_chats(driver)
+    print("Выполнение программы успешно завершено!!!")
