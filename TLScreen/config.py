c1e41beb5572ff75442f59e004812722aaa6e0c0
Sergey <46860359+killer544462@users.noreply.github.com>
2020-08-19 19:08:24 +0300

    Added exception handling in config

--- TLScreen/config.py
+++ TLScreen/config.py
@@ -1,4 +1,5 @@
 import os
+from logger import log
 
 
 class Config:
@@ -11,44 +12,69 @@ class Config:
         self.screen_channel = False
         self.screen_bot = False
         self.firefox_profile = os.environ["MProfile"]
-        self.download_folder = ".\\"
+        self.download_folder = "C:\\Download"
 
     def parse_config(self, name_config_file):
-        with open(name_config_file, "r") as file:
-            for line in file:
-                line = line.strip()
-                index_comment = line.find('#')
-                if index_comment != -1:
-                    line = line[:index_comment]
-                parametrs = line.split(':')
-                if len(parametrs) == 1:
-                    continue
-                values = parametrs[1].split()
-                if parametrs[0].strip() == "MAX_MESSAGES":
-                    if len(values) == 1:
-                        self.max_messages = int(values[0].strip())
-                elif parametrs[0].strip() == "FIREFOX_PROFILE":
-                    if len(values) == 1:
-                        self.firefox_profile = values[0]
-                elif parametrs[0].strip() == "DOWNLOAD_FOLDER":
-                    if len(values) == 1:
-                        self.download_folder = values[0]
-                elif parametrs[0].strip() == "SCREEN_UNREAD_MESSAGES":
-                    if len(values) == 1:
-                        self.screen_unread_messages = bool(values[0].strip())
-                elif parametrs[0].strip() == "SCREEN_GROUP":
-                    if len(values) == 1:
-                        self.screen_group = bool(values[0].strip())
-                elif parametrs[0].strip() == "SCREEN_CONTACT":
-                    if len(values) == 1:
-                        self.screen_contact = bool(values[0].strip())
-                elif parametrs[0].strip() == "SCREEN_CHANNEL":
-                    if len(values) == 1:
-                        self.screen_channel = bool(values[0].strip())
-                elif parametrs[0].strip() == "SCREEN_BOT":
-                    if len(values) == 1:
-                        self.screen_bot = bool(values[0].strip())
-                elif parametrs[0].strip() == "EXTENSIONS_DOCUMENT":
-                    self.extensions_document.clear()
-                    for value in values:
-                        self.extensions_document.append(value.strip())
+        try:
+            with open(name_config_file, "r") as file:
+                for line in file:
+                    line = line.strip()
+                    index_comment = line.find('#')
+                    if index_comment != -1:
+                        line = line[:index_comment]
+                    index_separator = line.find(':')
+
+                    if index_separator == -1 or (index_separator + 1) == len(line):
+                        continue
+
+                    key = line[:index_separator]
+                    value = line[(index_separator + 1):].split()
+
+                    if key.strip() == "MAX_MESSAGES":
+                        if len(value) == 1:
+                            self.max_messages = int(value[0].strip())
+                    elif key.strip() == "FIREFOX_PROFILE":
+                        if len(value) == 1:
+                            self.firefox_profile = value[0]
+                    elif key.strip() == "SCREEN_UNREAD_MESSAGES":
+                        if len(value) == 1:
+                            if value[0].strip() == "False":
+                                self.screen_unread_messages = False
+                            elif value[0].strip() == "True":
+                                self.screen_unread_messages = True
+                    elif key.strip() == "SCREEN_GROUP":
+                        if len(value) == 1:
+                            if value[0].strip() == "False":
+                                self.screen_group = False
+                            elif value[0].strip() == "True":
+                                self.screen_group = True
+                    elif key.strip() == "SCREEN_CONTACT":
+                        if len(value) == 1:
+                            if value[0].strip() == "False":
+                                self.screen_contact = False
+                            elif value[0].strip() == "True":
+                                self.screen_contact = True
+                    elif key.strip() == "SCREEN_CHANNEL":
+                        if len(value) == 1:
+                            if value[0].strip() == "False":
+                                self.screen_channel = False
+                            elif value[0].strip() == "True":
+                                self.screen_channel = True
+                    elif key.strip() == "SCREEN_BOT":
+                        if len(value) == 1:
+                            if value[0].strip() == "False":
+                                self.screen_bot = False
+                            elif value[0].strip() == "True":
+                                self.screen_bot = True
+                    elif key.strip() == "EXTENSIONS_DOCUMENT":
+                        self.extensions_document.clear()
+                        for v in value:
+                            self.extensions_document.append(v.strip())
+                    elif key.strip() == "DOWNLOAD_FOLDER":
+                        if len(value) == 1:
+                            self.download_folder = value[0]
+        except FileNotFoundError:
+            log.info(
+                "Конфиг файл config.txt не найден. Применены настройки по умолчанию")
+        except Exception:
+            log.info("Произошла ошибка в разборе конфиг файла config.txt")
