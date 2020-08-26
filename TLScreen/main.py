deefdb35005ce72c15f9a5fa419041e3c07e7135
Sergey <46860359+killer544462@users.noreply.github.com>
2020-08-26 10:31:28 +0300

    Review

--- TLScreen/main.py
+++ TLScreen/main.py
@@ -16,6 +16,7 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 URL = "https://web.telegram.org/#/im"
 
 
+# Проверка интернет соединения. Функция будет бесконечно ожидать подключения к интернету
 def check_internet_connection():
     try:
         urllib.request.urlopen("http://google.com")
@@ -25,9 +26,9 @@ def check_internet_connection():
         check_internet_connection()
     else:
         log.info("Интернет соединение установлено")
-        return
 
 
+# Настройка профиля браузера, под которым будет выполняться программа. (настроенный профиль)
 def create_firefox_profile(conf):
     fp = webdriver.FirefoxProfile(conf.firefox_profile)
     fp.set_preference("browser.download.folderList", 2)
@@ -40,150 +41,172 @@ def create_firefox_profile(conf):
     return fp
 
 
-def screen_chat(driver, conf):
-    chat_header = wait.until(EC.presence_of_element_located(
-        (By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/a")))
-    chat_header.click()
-
-    chat_type = driver.find_element_by_xpath(
-        "/html/body/div[5]/div[2]/div/div/div[1]/div[1]/div[2]").text
-
-    chat_name = driver.find_element_by_xpath(
-        "/html/body/div[5]/div[2]/div/div/div[1]/div[2]/div[2]/div[1]").text
-
-    chat_status = driver.find_element_by_xpath(
-        "/html/body/div[5]/div[2]/div/div/div[1]/div[2]/div[2]/div[2]").text
-
-    path_chat_folder = "{}{}".format(conf.download_folder, chat_name)
-
-    if os.path.isdir(path_chat_folder):
-        path_chat_folder = path_chat_folder + "_{}".format(uuid.uuid4())
-
-    path_chat_folder = path_chat_folder + "\\"
-
-    os.mkdir(path_chat_folder)
-    log.info("Создана папка path_chat_folder")
-
-    img = ImageGrab.grab()
-    img.save(path_chat_folder + "profile.jpg")
-    log.info(
-        f"Сделан снимок профиля собеседника {path_chat_folder}profile.jpg")
-
-    driver.find_element_by_xpath(
-        "/html/body/div[5]/div[2]/div/div/div[1]/div[1]/div[1]/a").click()
-
-    if chat_status == "bot" and not conf.screen_bot:
-        log.info(
-            f"Чат с {chat_name} был пропущен из-за настройки в config: SCREEN_BOT: False")
-        return
-
-    if chat_type == "Group info" and not conf.screen_group:
-        log.info(
-            f"Чат с {chat_name} был пропущен из-за настройки в config: SCREEN_GROUP: False")
-        return
-
-    if chat_type == "Contact info" and not conf.screen_contact:
-        log.info(
-            f"Чат с {chat_name} был пропущен из-за настройки в config: SCREEN_CONTACT: False")
-        return
-
-    if chat_type == "Channel info" and not conf.screen_channel:
-        log.info(
-            f"Чат с {chat_name} был пропущен из-за настройки в config: SCREEN_CHANNEL: False")
-        return
-
-    hystory_chat = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div")
-
-    hystory_chat_height = hystory_chat.size["height"]
-
-    # scount = 1
-    while True:
-        driver.execute_script(
-            "document.getElementsByClassName(\"im_history_scrollable_wrap\")[0].scroll(0, 0)")
-        time.sleep(0.5)
-        try:
-            WebDriverWait(driver, 40).until(EC.invisibility_of_element_located(
-                (By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]")))
-        except Exception as except_info:
-            break
-
-        num_messages = len(driver.find_elements_by_css_selector(
-            "div.im_history_messages_peer:not(.ng-hide) > div.im_history_message_wrap"))
-
-        if (hystory_chat_height == hystory_chat.size["height"]) or (conf.max_messages <= num_messages):
-            break
-
-        hystory_chat_height = hystory_chat.size["height"]
-
-    chat_height = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]").size["height"]
-
-    img = ImageGrab.grab()
-    img.save(path_chat_folder + "chat_0.jpg")
-    log.info(f"Сделан 0-й снимок диалога {path_chat_folder}chat_0.jpg")
-
-    screen_number = 1
-    while True:
-        scroll_height = chat_height * screen_number * 0.9
-        driver.execute_script(
-            "document.getElementsByClassName(\"im_history_scrollable_wrap\")[0].scroll({}, {})".format(0, scroll_height))
-        time.sleep(0.5)
-        img = ImageGrab.grab()
-        img.save(path_chat_folder + "\\chat_{}.jpg".format(screen_number))
-        log.info(f"{path_chat_folder}\chat_{screen_number}.jpg")
-
-        if scroll_height >= hystory_chat.size["height"]:
-            break
-        screen_number = screen_number + 1
-
-    log.info("Начало загрузки вложений")
-    audios = driver.find_elements_by_css_selector(
-        "div.im_history_messages_peer:not(.ng-hide) div.audio_player_actions > a:nth-child(1)")
-
-    download_audios = 0
-    for download_audio in audios:
-        try:
-            download_audio.click()
-            download_audios = download_audios + 1
-        except StaleElementReferenceException:
-            log.info("Ошибка загрузки аудио сообщения")
-
-    documents = driver.find_elements_by_css_selector(
-        "div.im_history_messages_peer:not(.ng-hide) .im_message_document")
-
-    download_documents = 0
-    for document in documents:
-        extension_document = document.find_element_by_xpath(".").find_element_by_class_name(
-            "im_message_document_name").get_attribute("data-ext")
-
-        if extension_document in conf.extensions_document:
-            try:
-                document.find_element_by_css_selector(
-                    "div.im_message_document_actions > a:nth-child(1)").click()
-                download_documents = download_documents + 1
-            except StaleElementReferenceException:
-                log.info("Ошибка загрузки документа")
-
-    count_documents = download_audios + download_documents
-    while True:
-        if len(os.listdir("{}TmpDownload".format(conf.download_folder))) == count_documents:
-            log.info(f"Загружено {count_documents} вложений")
-            break
+# Проверка на успешный вход (0: вход успешно выполнен; -1: ошибка)
+def check_login():
+    if driver.current_url != URL:
+        driver.get(URL)
+    try:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/ul/li[1]")))
+    except TimeoutException:
+        if driver.current_url == 'https://web.telegram.org/#/login':
+            log.info("Вход в телеграм не выполнен")
         else:
-            time.sleep(0.5)
+            log.info(
+                "Не удалось загрузить начальную страницу Telegram: https://web.telegram.org//#//im")
+        return -1
+    else:
+        return 0
 
-    for file in os.listdir("{}TmpDownload".format(conf.download_folder)):
-        os.rename("{}TmpDownload\\{}".format(conf.download_folder, file),
-                  path_chat_folder + "\\{}".format(file))
-    log.info(f"Загруженные файлы перемещены в {path_chat_folder}")
 
-
+# Снимки всех чатов (0: снимки сделаны успешно; -1: ошибка)
 def screen_chats(driver, conf):
+    if check_login():
+        return check_login
     if not os.path.isdir("{}TmpDownload".format(conf.download_folder)):
         os.mkdir("{}TmpDownload".format(conf.download_folder))
         log.info(f"папка {conf.download_folder}TmpDownload создана")
     chat_number = 0
+
+    def screen_chat():
+        print(driver.current_url)
+        chat_header = wait.until(EC.presence_of_element_located(
+            (By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/a")))
+        chat_header.click()
+
+        chat_type = driver.find_element_by_xpath(
+            "/html/body/div[5]/div[2]/div/div/div[1]/div[1]/div[2]").text
+
+        chat_name = driver.find_element_by_xpath(
+            "/html/body/div[5]/div[2]/div/div/div[1]/div[2]/div[2]/div[1]").text
+
+        chat_status = driver.find_element_by_xpath(
+            "/html/body/div[5]/div[2]/div/div/div[1]/div[2]/div[2]/div[2]").text
+
+        path_chat_folder = "{}{}".format(conf.download_folder, chat_name)
+
+        if os.path.isdir(path_chat_folder):
+            path_chat_folder = path_chat_folder + "_{}".format(uuid.uuid4())
+
+        path_chat_folder = path_chat_folder + "\\"
+
+        os.mkdir(path_chat_folder)
+        log.info("Создана папка path_chat_folder")
+
+        img = ImageGrab.grab()
+        img.save(path_chat_folder + "profile.jpg")
+        log.info(
+            f"Сделан снимок профиля собеседника {path_chat_folder}profile.jpg")
+
+        driver.find_element_by_xpath(
+            "/html/body/div[5]/div[2]/div/div/div[1]/div[1]/div[1]/a").click()
+
+        if chat_status == "bot" and not conf.screen_bot:
+            log.info(
+                f"Чат с {chat_name} был пропущен из-за настройки в config: SCREEN_BOT: False")
+            return
+
+        if chat_type == "Group info" and not conf.screen_group:
+            log.info(
+                f"Чат с {chat_name} был пропущен из-за настройки в config: SCREEN_GROUP: False")
+            return
+
+        if chat_type == "Contact info" and not conf.screen_contact:
+            log.info(
+                f"Чат с {chat_name} был пропущен из-за настройки в config: SCREEN_CONTACT: False")
+            return
+
+        if chat_type == "Channel info" and not conf.screen_channel:
+            log.info(
+                f"Чат с {chat_name} был пропущен из-за настройки в config: SCREEN_CHANNEL: False")
+            return
+
+        hystory_chat = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div")
+
+        hystory_chat_height = hystory_chat.size["height"]
+
+        # scount = 1
+        while True:
+            driver.execute_script(
+                "document.getElementsByClassName(\"im_history_scrollable_wrap\")[0].scroll(0, 0)")
+            time.sleep(0.5)
+            try:
+                WebDriverWait(driver, 40).until(EC.invisibility_of_element_located(
+                    (By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]")))
+            except Exception as except_info:
+                break
+
+            num_messages = len(driver.find_elements_by_css_selector(
+                "div.im_history_messages_peer:not(.ng-hide) > div.im_history_message_wrap"))
+
+            if (hystory_chat_height == hystory_chat.size["height"]) or (conf.max_messages <= num_messages):
+                break
+
+            hystory_chat_height = hystory_chat.size["height"]
+
+        chat_height = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]").size["height"]
+
+        img = ImageGrab.grab()
+        img.save(path_chat_folder + "chat_0.jpg")
+        log.info(f"Сделан 0-й снимок диалога {path_chat_folder}chat_0.jpg")
+
+        screen_number = 1
+        while True:
+            scroll_height = chat_height * screen_number * 0.9
+            driver.execute_script(
+                "document.getElementsByClassName(\"im_history_scrollable_wrap\")[0].scroll({}, {})".format(0, scroll_height))
+            time.sleep(0.5)
+            img = ImageGrab.grab()
+            img.save(path_chat_folder + "\\chat_{}.jpg".format(screen_number))
+            log.info(f"{path_chat_folder}\chat_{screen_number}.jpg")
+
+            if scroll_height >= hystory_chat.size["height"]:
+                break
+            screen_number = screen_number + 1
+
+        log.info("Начало загрузки вложений")
+        audios = driver.find_elements_by_css_selector(
+            "div.im_history_messages_peer:not(.ng-hide) div.audio_player_actions > a:nth-child(1)")
+
+        download_audios = 0
+        for download_audio in audios:
+            try:
+                download_audio.click()
+                download_audios = download_audios + 1
+            except StaleElementReferenceException:
+                log.info("Ошибка загрузки аудио сообщения")
+
+        documents = driver.find_elements_by_css_selector(
+            "div.im_history_messages_peer:not(.ng-hide) .im_message_document")
+
+        download_documents = 0
+        for document in documents:
+            extension_document = document.find_element_by_xpath(".").find_element_by_class_name(
+                "im_message_document_name").get_attribute("data-ext")
+
+            if extension_document in conf.extensions_document:
+                try:
+                    document.find_element_by_css_selector(
+                        "div.im_message_document_actions > a:nth-child(1)").click()
+                    download_documents = download_documents + 1
+                except StaleElementReferenceException:
+                    log.info("Ошибка загрузки документа")
+
+        count_documents = download_audios + download_documents
+        while True:
+            if len(os.listdir("{}TmpDownload".format(conf.download_folder))) == count_documents:
+                log.info(f"Загружено {count_documents} вложений")
+                break
+            else:
+                time.sleep(0.5)
+
+        for file in os.listdir("{}TmpDownload".format(conf.download_folder)):
+            os.rename("{}TmpDownload\\{}".format(conf.download_folder, file),
+                      path_chat_folder + "\\{}".format(file))
+        log.info(f"Загруженные файлы перемещены в {path_chat_folder}")
+
     while True:
         chat_number = chat_number + 1
 
@@ -198,11 +221,15 @@ def screen_chats(driver, conf):
         chat = driver.find_element_by_xpath(
             "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/ul/li[{}]".format(chat_number))
         chat.click()
-        screen_chat(driver, conf)
-    log.info("Выполнение программы успешно завершено!!!")
+        screen_chat()
+    return 0
 
 
+# Снимки заголовком всех чатов (0: снимки сделаны успешно; -1: ошибка)
 def screen_headers_chats(driver, conf):
+    if check_login():
+        return check_login
+
     slider = driver.find_element_by_xpath(
         "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div")
 
@@ -235,17 +262,14 @@ def screen_headers_chats(driver, conf):
     driver.execute_script(
         "document.getElementsByClassName(\"im_dialogs_scrollable_wrap  nano-content\")[0].scroll({}, {})".format(0, 0))
 
-    screen_chats(driver, conf)
+    return 0
+
+# Снимки основного профиля (0: снимки сделаны успешно; -1: ошибка)
 
 
 def screen_object_profile(driver, conf):
-    try:
-        wait.until(EC.presence_of_element_located(
-            (By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/ul/li[1]")))
-    except TimeoutException:
-        if driver.current_url == 'https://web.telegram.org/#/login':
-            log.info("Вход в телеграм не выполонен")
-            return
+    if check_login():
+        return check_login
 
     button_menu = wait.until(EC.presence_of_element_located(
         (By.XPATH, "/html/body/div[1]/div[1]/div/div/div[1]/div/a/div")))
@@ -284,8 +308,7 @@ def screen_object_profile(driver, conf):
         "/html/body/div[6]/div[2]/div/div/div[1]/div[1]/div/a").click()
     driver.find_element_by_xpath(
         "/html/body/div[5]/div[2]/div/div/div[1]/div[1]/div[1]/a[1]").click()
-
-    screen_headers_chats(driver, conf)
+    return 0
 
 
 if __name__ == "__main__":
@@ -298,4 +321,16 @@ if __name__ == "__main__":
     check_internet_connection()
     wait = WebDriverWait(driver, 40)
     driver.get(URL)
-    screen_object_profile(driver, conf)
+    if screen_object_profile(driver, conf):
+        sys.exit()
+    else:
+        log.info("Снимки профиля выполнены")
+    if screen_headers_chats(driver, conf):
+        sys.exit()
+    else:
+        log.info("Снимки заголовков чатов выполнены")
+    if screen_chats(driver, conf):
+        sys.exit()
+    else:
+        log.info("Снимки чатов выполнены")
+    log.info("Выполнение программы успешно завершено!!!")
